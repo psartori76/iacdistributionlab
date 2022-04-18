c0d82cefb2431da7053ba230e34ec81e4924490a
PSARTORI <PSARTORI@PSARTORI-7420.al.oracle.com>
2022-04-18 10:40:55 -0300

    initial commit

--- demo.tf
+++ demo.tf
@@ -1,19 +1,13 @@
-variable "compartment_ocid" {
-    default = "ocid1.compartment.oc1..aaaaaaaawa2imxuftedpg2dknhmkqanyz7go6srvenlkjpq7oful5folwlzq"
-}
-variable "region" {
-    default = "us-ashburn-1"
-}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "bucket_name" {}
 
 provider "oci" {
-  region = var.region
+    region = var.region
 }
 
-variable "bucket_namespace" {
-    default = "id0cvlngwjnu"
-}
-variable "bucket_name" {
-    default = "resource_manager_demo_bucket"
+data "oci_objectstorage_namespace" "namespace" {
+    compartment_id = var.compartment_ocid
 }
 
 resource "oci_objectstorage_bucket" "create_bucket" {
